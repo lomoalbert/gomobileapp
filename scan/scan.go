b0e41460f289f874c1c84b333a86f39ceb5b418f
albert <lomoalbert@gmail.com>
2015-07-19 14:10:57 +0800

    添加cube文件

--- scan/scan.go
+++ scan/scan.go
@@ -71,13 +71,17 @@ func start() {
     buf = gl.CreateBuffer()
     gl.BindBuffer(gl.ARRAY_BUFFER, buf)
     gl.BufferData(gl.ARRAY_BUFFER, triangleData, gl.STATIC_DRAW)
-    /*
+
+    /*opengl中三种变量
     uniform变量是外部application程序传递给（vertex和fragment）shader的变量。因此它是application通过函数glUniform**（）函数赋值的。
     在（vertex和fragment）shader程序内部，uniform变量就像是C语言里面的常量（const ），它不能被shader程序修改。（shader只能用，不能改）
 
     attribute变量是只能在vertex shader中使用的变量。（它不能在fragment shader中声明attribute变量，也不能被fragment shader中使用）
     一般用attribute变量来表示一些顶点的数据，如：顶点坐标，法线，纹理坐标，顶点颜色等。
     在application中，一般用函数glBindAttribLocation（）来绑定每个attribute变量的位置，然后用函数glVertexAttribPointer（）为每个attribute变量赋值。
+
+    varying变量是vertex和fragment shader之间做数据传递用的。一般vertex shader修改varying变量的值，然后fragment shader使用该varying变量的值。
+    因此varying变量在vertex和fragment shader二者之间的声明必须是一致的。application不能使用此变量。
     */
     position = gl.GetAttribLocation(program, "position")//获取位置对象(索引)
     color =  gl.GetUniformLocation(program, "color") // 获取颜色对象(索引)
